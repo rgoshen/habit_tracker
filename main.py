e3d3a6a0a2957defc4c475225ce03f4822ecb4a9
Richard Goshen <rick.goshen@gmail.com>
2020-12-09 09:05:18 -0700

    added update pixel

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
+from datetime import datetime
 
 USERNAME = "rickgoshen"
 TOKEN = "askdlfoiwssj3489alskdj89ajkg04jlauzlkx9g0a"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users/"
+GRAPH_ID = "graph1"
+today = datetime.now()
 
 # create user
 user_endpoint = f"{PIXELA_ENDPOINT}"
@@ -25,7 +28,7 @@ headers = {
 }
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Minutes Coding",
     "unit": "mins",
     "type": "int",
@@ -35,4 +38,13 @@ graph_config = {
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response)
 
+# create a new pixel on habit graph
+pixel_creation_endpoint = f"{PIXELA_ENDPOINT}{USERNAME}/graphs/{GRAPH_ID}"
 
+pixel_data = {
+    "date": today.strfttime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today? ")
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response)
