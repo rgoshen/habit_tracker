c0765319095249487ca475cb7a700ac5b5c1d0fb
Richard Goshen <rick.goshen@gmail.com>
2020-12-09 08:42:00 -0700

    created pixela graph

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import requests
 
-pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "rickgoshen"
+TOKEN = "askdlfoiwssj3489alskdj89ajkg04jlauzlkx9g0a"
+PIXELA_ENDPOINT = "https://pixe.la/v1/users/"
+
+# create user
+user_endpoint = f"{PIXELA_ENDPOINT}"
 
 user_params = {
-    "token": "askdlfoiwssj3489alskdj89ajkg04jlauzlkx9g0a",
-    "username": "rickgoshen",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-# create user
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+# create a pixels graph
+graph_endpoint = f"{PIXELA_ENDPOINT}{USERNAME}/graphs"
+
+headers = {
+    "X-USER-TOKEN":TOKEN
+}
+
+graph_config = {
+    "id": "graph1",
+    "name": "Minutes Coding",
+    "unit": "mins",
+    "type": "int",
+    "color": "sora"
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response)
+
+
